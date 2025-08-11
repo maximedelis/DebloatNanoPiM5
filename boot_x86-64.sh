294fe5b34077ee8a1b2118b99478829e14c862d6
MaxDel <contact@maxdel.ovh>
2025-08-11 14:28:03 +0200

    Added x86-64 version

---
+++ boot_x86-64.sh
@@ -0,0 +1,67 @@
+mediab="boot.img"
+size="100M"
+
+
+
+### Create empty img
+truncate -s "$size" "$mediab"
+
+cat <<-EOF | /usr/sbin/sfdisk "$mediab"
+label: gpt
+unit: sectors
+first-lba: 34
+part1: start=32768, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name=boot
+EOF
+sync
+
+lodev_boot="$(sudo /usr/sbin/losetup -f)"
+sudo losetup -vP "$lodev_boot" "$mediab" && sync
+sudo mkfs.ext4 -L boot -vO metadata_csum_seed "${lodev_boot}p1" && sync
+
+# Mount
+sudo mkdir -p mnt/boot
+sudo mount -o rw "${lodev_boot}p1" mnt/boot
+
+
+### Download Linux Kernel from Inindev
+latest='https://api.github.com/repos/inindev/linux-rockchip/releases/latest'
+kurl=$(curl -s "$latest" | grep 'browser_download_url' | grep 'linux-image' | grep -v 'dbg' | grep -o 'https://[^"]*')
+kfile="$(basename $kurl)"
+wget "$kurl"
+
+sudo cp "$kfile" mnt/tmp/kernel.deb
+sudo chroot mnt dpkg -i '/tmp/kernel.deb'
+sudo mkdir -p 'mnt/boot/extlinux'
+linux_version=$(sudo chroot mnt linux-version list)
+echo -e "
+menu title u-boot menu
+prompt 0
+default Linux
+timeout 30
+
+LABEL Linux
+\tlinux /boot/vmlinuz-$linux_version
+\tinitrd /boot/initrd-$linux_version
+\tfdt /boot/rk3576-nanopi-m5.dtb
+\tappend root=$(sudo chroot mnt blkid /dev/loop18p1 | grep -oPe '\s\KUUID="[^"]*"')" | sudo tee mnt/boot/extlinux/extlinux.conf
+
+
+# DTB
+sudo cp uboot/rk3576-nanopi-m5.dtb mnt/boot/rk3576-nanopi-m5.dtb
+
+# Setup fstab
+
+echo -e "
+$(sudo chroot mnt blkid /dev/loop18p1 | grep -oPe '\s\KUUID="[^"]*"' | sed "s/\"//g") /     ext4 noatime 0 1
+$(sudo chroot mnt blkid /dev/loop19p1 | grep -oPe '\s\KUUID="[^"]*"' | sed "s/\"//g") /boot ext4 noatime 0 1" | sudo tee mnt/etc/fstab
+
+### Umount and install U-Boot
+
+sudo umount mnt/boot
+
+sudo dd if=uboot/idbloader of="${lodev_boot}" seek=64 bs=512 && sync
+sudo dd if=uboot/u-boot.itb of="${lodev_boot}" seek=16384 bs=512 && sync
+
+sudo losetup -d "${lodev_boot}"
+
+# chmod 444 "$mediab"
---
+++ rootfs_x86-64.sh
@@ -0,0 +1,92 @@
+media="rootfs.img"
+size="2g"
+deb_dist='bookworm'
+deb_src="http://ftp.fr.debian.org/debian/"
+
+
+
+
+### Create empty img
+truncate -s "$size" "$media"
+
+cat <<-EOF | /usr/sbin/sfdisk "$media"
+label: gpt
+unit: sectors
+first-lba: 2048
+part1: start=32768, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name=rootfs
+EOF
+sync
+
+lodev_rootfs="$(sudo /usr/sbin/losetup -f)"
+sudo losetup -vP "$lodev_rootfs" "$media" && sync
+sudo mkfs.ext4 -L rootfs -vO metadata_csum_seed "${lodev_rootfs}p1" && sync
+
+# Mount
+mkdir -p mnt
+sudo mount -o rw "${lodev_rootfs}p1" mnt
+sudo mkdir -p mnt/sys/ && sudo mount --bind /sys/ mnt/sys/
+sudo mkdir -p mnt/proc/ && sudo mount --bind /proc/ mnt/proc/
+sudo mkdir -p mnt/dev/ && sudo mount --bind /dev/ mnt/dev/
+
+
+
+
+### Install system
+pkgs="initramfs-tools, dbus, dhcpcd, libpam-systemd, openssh-server, systemd-timesyncd, rfkill, wireless-regdb, wpasupplicant, \
+bc, curl, pciutils, sudo, unzip, wget, xxd, xz-utils, zip, zstd"
+
+#TODO: cache
+
+sudo debootstrap --arch arm64 --include "$pkgs" --exclude "isc-dhcp-client" "$deb_dist" mnt "$deb_src"
+
+
+### x86-64 specifics
+
+sudo apt install qemu-user-static
+sudo cp /usr/bin/qemu-aarch64-static mnt/usr/bin
+
+### Config system
+
+user="debian"
+pswd="debian"
+hostname="NanoPiM5"
+
+sudo chroot mnt/ qemu-aarch64-static /usr/sbin/useradd -m "$user" -s '/bin/bash'
+sudo chroot mnt/ qemu-aarch64-static /bin/sh -c "/usr/bin/echo $user:$pswd | /usr/sbin/chpasswd -c YESCRYPT"
+sudo chroot mnt/ qemu-aarch64-static /usr/bin/passwd -e "$user"
+(umask 377 && echo "$user ALL=(ALL) NOPASSWD: ALL" | sudo tee "mnt/etc/sudoers.d/$user")
+
+echo $hostname | sudo tee "mnt/etc/hostname"
+sudo sed -i "s/127.0.0.1\tlocalhost/127.0.0.1\tlocalhost\n127.0.1.1\t$hostname/" "mnt/etc/hosts"
+
+#Startup script
+sudo install -Dvm 754 'rc.local' "mnt/etc/rc.local"
+
+
+
+
+### Cleanup
+
+#Regen machine-id
+sudo truncate -s0 "mnt/etc/machine-id"
+
+#Off sshd until regen key
+sudo rm -fv "mnt/etc/systemd/system/sshd.service"
+sudo rm -fv "mnt/etc/systemd/system/multi-user.target.wants/ssh.service"
+
+### Boot img
+
+./boot_x86-64.sh
+
+sudo rm -rf mnt/boot
+sudo rm mnt/usr/bin/qemu-aarch64-static
+
+sudo fstrim -v mnt
+
+sudo umount mnt/proc
+sudo umount mnt/sys
+sudo umount mnt/dev
+sudo umount mnt
+sudo losetup -d "$lodev_rootfs"
+
+# chmod 444 "$media"
